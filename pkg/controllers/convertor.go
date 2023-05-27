4981944ac34adcca137c4cc42decee210ed6b4e4
Nusaib <muhammednusaib44@gmail.com>
2023-05-27 16:14:37 +0530

    final commit

--- pkg/controllers/convertor.go
+++ pkg/controllers/convertor.go
@@ -26,40 +26,103 @@ type Trait struct {
 
 func convertRequest(req Body) ConvertedRequest {
 	convertedReq := ConvertedRequest{
-		Event:           req.ev,
-		EventType:       req.et,
-		AppID:           req.id,
-		UserID:          req.uid,
-		MessageID:       req.mid,
-		PageTitle:       req.t,
-		PageURL:         req.p,
-		BrowserLanguage: req.l,
-		ScreenSize:      req.sc,
-		Attributes: map[string]Attribute{
-			req.atrk1: {
-				Value: req.atrv1,
-				Type:  req.atrt1,
-			},
-			req.atrk2: {
-				Value: req.atrv2,
-				Type:  req.atrt2,
-			},
-		},
-		Traits: map[string]Trait{
-			req.uatrk1: {
-				Value: req.uatrv1,
-				Type:  req.uatrt1,
-			},
-			req.uatrk2: {
-				Value: req.uatrv2,
-				Type:  req.uatrt2,
-			},
-			req.uatrk3: {
-				Value: req.uatrv3,
-				Type:  req.uatrt3,
-			},
-		},
+		Event:           req.Ev,
+		EventType:       req.Et,
+		AppID:           req.ID,
+		UserID:          req.UID,
+		MessageID:       req.MID,
+		PageTitle:       req.T,
+		PageURL:         req.P,
+		BrowserLanguage: req.L,
+		ScreenSize:      req.SC,
+		Attributes:      make(map[string]Attribute),
+		Traits:          make(map[string]Trait),
 	}
+
+	if req.AtRk1 != "" {
+		convertedReq.Attributes[req.AtRk1] = Attribute{
+			Value: req.AtRv1,
+			Type:  req.AtRt1,
+		}
+	}
+	if req.AtRk2 != "" {
+		convertedReq.Attributes[req.AtRk2] = Attribute{
+			Value: req.AtRv2,
+			Type:  req.AtRt2,
+		}
+	}
+	if req.AtRk3 != "" {
+		convertedReq.Attributes[req.AtRk3] = Attribute{
+			Value: req.AtRv3,
+			Type:  req.AtRt3,
+		}
+	}
+	if req.AtRk4 != "" {
+		convertedReq.Attributes[req.AtRk4] = Attribute{
+			Value: req.AtRv4,
+			Type:  req.AtRt4,
+		}
+	}
+	if req.UaRk1 != "" {
+		convertedReq.Traits[req.UaRk1] = Trait{
+			Value: req.UaRv1,
+			Type:  req.UaRt1,
+		}
+	}
+	if req.UaRk2 != "" {
+		convertedReq.Traits[req.UaRk2] = Trait{
+			Value: req.UaRv2,
+			Type:  req.UaRt2,
+		}
+	}
+	if req.UaRk3 != "" {
+		convertedReq.Traits[req.UaRk3] = Trait{
+			Value: req.UaRv3,
+			Type:  req.UaRt3,
+		}
+	}
+	if req.UaRk4 != "" {
+		convertedReq.Traits[req.UaRk4] = Trait{
+			Value: req.UaRv4,
+			Type:  req.UaRt4,
+		}
+	}
+	if req.UaRk5 != "" {
+		convertedReq.Traits[req.UaRk5] = Trait{
+			Value: req.UaRv5,
+			Type:  req.UaRt5,
+		}
+	}
+	if req.UaRk6 != "" {
+		convertedReq.Traits[req.UaRk6] = Trait{
+			Value: req.UaRv6,
+			Type:  req.UaRt6,
+		}
+	}
+
+	// 	req.AtRk1: {
+	// 		Value: req.AtRv1,
+	// 		Type:  req.AtRt1,
+	// 	},
+	// 	req.AtRk2: {
+	// 		Value: req.AtRv2,
+	// 		Type:  req.AtRt2,
+	// 	},
+	// },
+	// {
+	// 	req.UaRk1: {
+	// 		Value: req.UaRv1,
+	// 		Type:  req.UaRt1,
+	// 	},
+	// 	req.UaRt2: {
+	// 		Value: req.UaRv2,
+	// 		Type:  req.UaRt2,
+	// 	},
+	// 	req.UaRt3: {
+	// 		Value: req.UaRv3,
+	// 		Type:  req.UaRt3,
+	// 	},
+	// },
 
 	return convertedReq
 }
--- pkg/controllers/main.go
+++ pkg/controllers/main.go
@@ -8,41 +8,57 @@ import (
 )
 
 type Body struct {
-	ev   string 
-	et    string 
-	id   string 
-	uid  string 
-	mid   string 
-	t     string 
-	p     string 
-	l     string 
-	sc   string 
-	atrk1 string 
-	atrv1 string 
-	atrt1 string 
-	atrk2 string 
-	atrv2 string 
-	atrt2 string 
-	uatrk1 string 
-	uatrv1 string 
-	uatrt1 string 
-	uatrk2 string 
-	uatrv2 string 
-	uatrt2 string 
-	uatrk3 string 
-	uatrv3 string 
-	uatrt3 string 
+	Ev    string `json:"ev"`
+	Et    string `json:"et"`
+	ID    string `json:"id"`
+	UID   string `json:"uid"`
+	MID   string `json:"mid"`
+	T     string `json:"t"`
+	P     string `json:"p"`
+	L     string `json:"l"`
+	SC    string `json:"sc"`
+	AtRk1 string `json:"atrk1"`
+	AtRv1 string `json:"atrv1"`
+	AtRt1 string `json:"atrt1"`
+	AtRk2 string `json:"atrk2"`
+	AtRv2 string `json:"atrv2"`
+	AtRt2 string `json:"atrt2"`
+	AtRk3 string `json:"atrk3"`
+	AtRv3 string `json:"atrv3"`
+	AtRt3 string `json:"atrt3"`
+	AtRk4 string `json:"atrk4"`
+	AtRv4 string `json:"atrv4"`
+	AtRt4 string `json:"atrt4"`
+	UaRk1 string `json:"uatrk1"`
+	UaRv1 string `json:"uatrv1"`
+	UaRt1 string `json:"uatrt1"`
+	UaRk2 string `json:"uatrk2"`
+	UaRv2 string `json:"uatrv2"`
+	UaRt2 string `json:"uatrt2"`
+	UaRk3 string `json:"uatrk3"`
+	UaRv3 string `json:"uatrv3"`
+	UaRt3 string `json:"uatrt3"`
+	UaRk4 string `json:"uatrk4"`
+	UaRv4 string `json:"uatrv4"`
+	UaRt4 string `json:"uatrt4"`
+	UaRk5 string `json:"uatrk5"`
+	UaRv5 string `json:"uatrv5"`
+	UaRt5 string `json:"uatrt5"`
+	UaRk6 string `json:"uatrk6"`
+	UaRv6 string `json:"uatrv6"`
+	UaRt6 string `json:"uatrt6"`
 }
+
+var Req Body
 
 func Incoming(g *gin.Context) {
 
 	requests := make(chan Body)
-	var req Body
 
-	go worker(g,requests)
+	go worker(g, requests)
 
-	if err := g.Bind(&req); err != nil {
+	if err := g.Bind(&Req); err != nil {
 		common.ErrorMessage(g, "failed to bind", err.Error(), http.StatusBadRequest)
 	}
-	requests <- req
+	requests <- Req
 }
